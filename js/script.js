8b159b070e3696b1e463f41791c80ba64ab79e83
Filippo Garota <filogarota101@gmail.com>
2021-03-18 16:04:22 +0100

    Inserisco il template nella lista

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 //Come abbiamo fatto questa mattina insieme dovete creare una todo list che permatta di aggiungere(tramite campo input) e rimuovere gli elementi!
 
-alert();
+//creo un array per la lista
+
+var list = ['fare la spesa', 'pulire il garage', 'portare a spasso il cane'];
+
+
+for (var i = 0; i < list.length; i++) {
+
+//clono il template
+var template = $('.template li').clone();
+//inserisco il contenuto dell'array nel template
+template.prepend(list[i]);
+//inserisco li nella lista
+$('.todo-list').append(template);
+}
+
+
+
